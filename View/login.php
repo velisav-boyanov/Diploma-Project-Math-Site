b73ef3ed2154079bcf2817405b09452ee11c6b79
0NZ1T4M <velislav.boyanov@gmail.com>
2020-12-01 12:42:41 +0200

    Added login and main page.

---
+++ View/login.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+<h1>Login.</h1>
+<form action="index.php?target=player&action=add" method="post">
+    <div class="form-group">
+        <label>Health(Max 4):</label>
+        <input type="text" name = "Health" class="form-control" required>
+        <div class="form-group">
+            <label>Starting X:</label>
+            <input type="text" name = "X" class="form-control" required>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary"> Start </button>
+            </div>
+</form>
+</body>
+</html>
---
+++ View/register.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+</head>
+<body>
+<h1>Register.</h1>
+<form action="index.php?target=user&action=add" method="post">
+    <div class="form-group">
+        <label>Username:</label>
+           <input type="text" name = "Username" class="form-control" required>
+           <div class="form-group">
+        <label>Email:</label>
+           <input type="text" name = "Email" class="form-control" required>
+           <div class="form-group">
+        <label>Password:</label>
+           <input type="text" name = "Password" class="form-control" required>
+           <div class="form-group">
+        <button type="submit" class="btn btn-primary"> Start </button>
+    </div>
+</form>
+</body>
+</html>
